e18f47f33be39fa8f1ae4fab819432f5bbf494df
horakf <horak.phil@gmail.com>
2024-07-08 01:05:51 +0200

    Add file_export with corresponding tests.
    Add functionality to save array data into numpy file on given path.

---
+++ src/data_clusterer/file_export.py
@@ -0,0 +1,33 @@
+import os
+from typing import Any
+
+import numpy as np
+import numpy.typing as npt
+
+
+def create_directories(file_path: str) -> None:
+    """Creates missing directories on given path to file.
+
+    Args:
+        file_path (str): Path to the target file.
+    Returns:
+        None
+    """
+    directory = os.path.dirname(file_path)
+    os.makedirs(directory, exist_ok=True)
+
+
+def save_numpy_array(file_path: str, data: npt.NDArray[Any]) -> None:
+    """Saves numpy array data into numpy file.
+
+    Args:
+        file_path (str): Path to the target file.
+        data (npt.NDArray): Data to be exported.
+    Returns:
+        None
+    """
+    create_directories(file_path)
+    np.save(file_path, data)
+
+
+def save_json_file(file_path: str): ...
