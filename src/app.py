decd5d96222882eac0f614e9f4c9d474c191d62f
horakf <horak.phil@gmail.com>
2024-07-08 13:04:11 +0200

    Add application launcher.

---
+++ src/app.py
@@ -0,0 +1,36 @@
+import argparse
+
+import numpy.typing as npt
+from data_clusterer.container import Container
+from dependency_injector.wiring import Provide, inject
+
+
+def main(config_file: str) -> None:
+    container = Container()
+    container.config.from_yaml(config_file)
+    data = container.load_data()
+    if data is None:
+        print("No data to process.")
+        return
+
+    container.wire(modules=[__name__])
+    labels = run_clustering(data)
+    container.save_data(data=labels)
+
+
+@inject
+def run_clustering(data: npt.NDArray, algorithm=Provide[Container.algorithm]):
+    model = algorithm
+    labels = model.fit(data)
+    return labels
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Data clustering application."
+    )
+    parser.add_argument(
+        "config_file", type=str, help="Path to the configuration file."
+    )
+    args = parser.parse_args()
+    main(args.config_file)
