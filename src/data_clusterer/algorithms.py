609b8a3f3b585b76bb281e086cbf994b6d645659
horakf <horak.phil@gmail.com>
2024-07-08 22:23:40 +0200

    Add implementation of DBSCAN algorithm.
    Add class for setting up and running DBSCAN algorithm and corresponding tests.

--- src/data_clusterer/algorithms.py
+++ src/data_clusterer/algorithms.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 import numpy.typing as npt
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN, KMeans
 
 
 class KMeansAlgorithm:
@@ -48,7 +48,48 @@ class KMeansAlgorithm:
         return self.model.fit_predict(data)
 
 
-class DBSCANAlgorithm: ...
+class DBSCANAlgorithm:
+    """Class for setting up and running DBSCAN clustering algorithm.
+
+    Attributes:
+        model (DBSCAN): An instance of a DBSCAN object.
+
+    """
+
+    def __init__(self, **kwargs) -> None:
+        """Initialization of the model and hyperparameters' attributes.
+
+        First model parameter is initialized with DBSCAN object. Then,
+        non-None values passed as keywords arguments are assigned to the
+        corresponding DBSCAN class attributes. If any attribute is not set,
+        default settings of DBSCAN class is used for the attribute.
+
+        Args:
+            **kwargs (dict): Keyword arguments containing name - value pairs
+            for setting DBSCAN hyperparameters.
+        Returns:
+            None
+
+        """
+        self.model = DBSCAN()
+        for key, value in kwargs.items():
+            if not value:
+                continue
+            setattr(self.model, key, value)
+
+    def fit(self, data: npt.NDArray[Any]) -> npt.NDArray[Any]:
+        """Function used for running DBSCAN clustering algorithm on given data.
+
+        Compute cluster centers and predict cluster index for each sample in
+        the input data.
+
+        Args:
+            data (npt.NDArray): Numpy array containing input data
+        Returns:
+            npt.NDArray: Array containing cluster label for each feature (array
+            column) of the input data.
+        """
+        return self.model.fit_predict(data)
 
 
 class AglomerativeAlgorithm: ...
--- src/tests/test_algorithms.py
+++ src/tests/test_algorithms.py
@@ -3,12 +3,17 @@ from typing import Any, Dict
 import numpy as np
 import numpy.typing as npt
 import pytest
-from data_clusterer.algorithms import KMeansAlgorithm
+from data_clusterer.algorithms import DBSCANAlgorithm, KMeansAlgorithm
 
 
 @pytest.fixture
 def kmeans_object():
     return KMeansAlgorithm(n_clusters=3, random_state=42)
+
+
+@pytest.fixture
+def dbscan_object():
+    return DBSCANAlgorithm(eps=4, min_samples=3)
 
 
 @pytest.mark.parametrize(
@@ -50,3 +55,32 @@ def test_kmeans_fit_number_of_clusters(
 def test_kmeans_fit_empty_data(kmeans_object: KMeansAlgorithm):
     with pytest.raises(ValueError):
         kmeans_object.fit(np.array([]).reshape(0, 2))
+
+
+@pytest.mark.parametrize(
+    "given_kwargs, expected_eps, expected_min_samples",
+    [
+        ({"eps": 4, "min_samples": 3}, 4, 3),
+        ({"eps": None, "min_samples": 2}, 0.5, 2),
+        ({"eps": None, "min_samples": None}, 0.5, 5),
+    ],
+)
+def test_dbscan_init(
+    given_kwargs: Dict[str, int | None],
+    expected_eps: int,
+    expected_min_samples: int | None,
+):
+    test_instance = DBSCANAlgorithm(**given_kwargs)
+    assert test_instance.model.eps == expected_eps
+    assert test_instance.model.min_samples == expected_min_samples
+
+
+def test_dbscan_fit_labels_length(
+    dbscan_object: DBSCANAlgorithm, test_data: npt.NDArray[Any]
+):
+    assert len(dbscan_object.fit(test_data)) == len(test_data)
+
+
+def test_dbscan_fit_empty_data(dbscan_object: DBSCANAlgorithm):
+    with pytest.raises(ValueError):
+        dbscan_object.fit(np.array([]).reshape(0, 2))
