297f7436609ee301356f9d5575e1314b24c4095d
horakf <horak.phil@gmail.com>
2024-07-08 23:33:23 +0200

    Add implementation of Aglomerative clustering algorithm.
    Add class for setting up and running Aglomerative clustering algorithm and corresponding tests.

--- src/data_clusterer/algorithms.py
+++ src/data_clusterer/algorithms.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 import numpy.typing as npt
-from sklearn.cluster import DBSCAN, KMeans
+from sklearn.cluster import DBSCAN, AgglomerativeClustering, KMeans
 
 
 class KMeansAlgorithm:
@@ -92,4 +92,48 @@ class DBSCANAlgorithm:
         return self.model.fit_predict(data)
 
 
-class AglomerativeAlgorithm: ...
+class AglomerativeAlgorithm:
+    """Class for setting up and running AgglomerativeClustering algorithm.
+
+    Attributes:
+        model (AgglomerativeClustering): An instance of an
+        AgglomerativeClustering object.
+
+    """
+
+    def __init__(self, **kwargs) -> None:
+        """Initialization of the model and hyperparameters' attributes.
+
+        First model parameter is initialized with AgglomerativeClustering
+        object. Then, non-None values passed as keywords arguments are
+        assigned to the corresponding AgglomerativeClustering class
+        attributes. If any attribute is not set, default settings of
+        AgglomerativeClustering class is used for the attribute.
+
+        Args:
+            **kwargs (dict): Keyword arguments containing name - value pairs
+            for setting AgglomerativeClustering hyperparameters.
+        Returns:
+            None
+
+        """
+        self.model = AgglomerativeClustering()
+        for key, value in kwargs.items():
+            if not value and key != "n_clusters":
+                continue
+            setattr(self.model, key, value)
+
+    def fit(self, data: npt.NDArray[Any]) -> npt.NDArray[Any]:
+        """Function used for running AgglomerativeClustering algorithm on
+        given data.
+
+        Compute cluster centers and predict cluster index for each sample in
+        the input data.
+
+        Args:
+            data (npt.NDArray): Numpy array containing input data
+        Returns:
+            npt.NDArray: Array containing cluster label for each feature (array
+            column) of the input data.
+        """
+        return self.model.fit_predict(data)
--- src/tests/test_algorithms.py
+++ src/tests/test_algorithms.py
@@ -3,7 +3,11 @@ from typing import Any, Dict
 import numpy as np
 import numpy.typing as npt
 import pytest
-from data_clusterer.algorithms import DBSCANAlgorithm, KMeansAlgorithm
+from data_clusterer.algorithms import (
+    AglomerativeAlgorithm,
+    DBSCANAlgorithm,
+    KMeansAlgorithm,
+)
 
 
 @pytest.fixture
@@ -14,6 +18,19 @@ def kmeans_object():
 @pytest.fixture
 def dbscan_object():
     return DBSCANAlgorithm(eps=4, min_samples=3)
+
+
+@pytest.fixture
+def aglomerative_object():
+    return AglomerativeAlgorithm(n_clusters=3, linkage="ward")
+
+
+@pytest.fixture
+def aglomerative_hyperparameters():
+    return {
+        "n_clusters": None,
+        "distance_threshold": 5,
+    }
 
 
 @pytest.mark.parametrize(
@@ -84,3 +101,55 @@ def test_dbscan_fit_labels_length(
 def test_dbscan_fit_empty_data(dbscan_object: DBSCANAlgorithm):
     with pytest.raises(ValueError):
         dbscan_object.fit(np.array([]).reshape(0, 2))
+
+
+@pytest.mark.parametrize(
+    "given_kwargs, expected_n_clusters, expected_linkage",
+    [
+        ({"n_clusters": 3, "linkage": "ward"}, 3, "ward"),
+        ({"n_clusters": None, "linkage": "single"}, None, "single"),
+        ({"n_clusters": None, "linkage": None}, None, "ward"),
+    ],
+)
+def test_aglomerative_init(
+    given_kwargs: Dict[str, int | None],
+    expected_n_clusters: int,
+    expected_linkage: int | None,
+):
+    test_instance = AglomerativeAlgorithm(**given_kwargs)
+    assert test_instance.model.n_clusters == expected_n_clusters
+    assert test_instance.model.linkage == expected_linkage
+
+
+def test_aglomerative_fit_labels_length(
+    aglomerative_object: AglomerativeAlgorithm, test_data: npt.NDArray[Any]
+):
+    assert len(aglomerative_object.fit(test_data)) == len(test_data)
+
+
+@pytest.mark.parametrize(
+    "n_clusters, expected_clusters", [(3, [0, 1, 2]), (5, [0, 1, 2, 3, 4])]
+)
+def test_aglomerative_fit_number_of_clusters(
+    test_data: npt.NDArray[Any], n_clusters: int, expected_clusters: list[int]
+):
+    test_instance = AglomerativeAlgorithm(n_clusters=n_clusters)
+    assert all(
+        label in expected_clusters for label in test_instance.fit(test_data)
+    )
+
+
+def test_aglomerative_fit_empty_data(
+    aglomerative_object: AglomerativeAlgorithm,
+):
+    with pytest.raises(ValueError):
+        aglomerative_object.fit(np.array([]).reshape(0, 2))
+
+
+def test_aglomerative_none_clusters(
+    aglomerative_hyperparameters: Dict[str, Any], test_data: npt.NDArray[Any]
+):
+    test_instance = AglomerativeAlgorithm(**aglomerative_hyperparameters)
+    assert test_instance.model.n_clusters is None
+    assert test_instance.model.distance_threshold == 5
+    assert len(test_instance.fit(test_data)) == len(test_data)
